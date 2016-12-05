cd9b4366c40836e28ce8c30847720036889df28b
Ben Lindsay <benjlindsay@gmail.com>
2016-12-04 21:49:53 -0500

    added a git pull step to deploy.sh to make it more robust for multiple users (untested)

--- deploy.sh
+++ deploy.sh
@@ -4,15 +4,21 @@
 #
 # Copyright (c) 2016 Ben Lindsay <benjlindsay@gmail.com>
 
-jekyll build
-
+# change directory to site content directory
 cd _site
 
 if [ ! -d .git ]; then
+  # initialize git repo if _site doesn't already contain one
   git init
   git remote add origin https://github.com/penndsg/penndsg.github.io.git
 fi
 
+# update _site repo to latest commit
+git reset --hard
+git pull
+# regenerate site content
+jekyll build
+# commmit changes and push to remote
 git add .
 git commit -m "Site updated on $(date)"
 git push origin master
